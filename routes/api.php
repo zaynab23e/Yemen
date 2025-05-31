196124735fdb94b3227c35f4d1188a92e34c1de2
aHMEdaLaa4 <kscb912@gmail.com>
2025-05-31 11:47:51 +0300

    Add admin authentication and management functionality
    - Implement AdminsAuthController for admin login, registration, and logout.
    - Create ItemsController for managing items with CRUD operations.
    - Add MenusCategoryController for managing menu categories.
    - Introduce HttpResponses trait for standardized API responses.
    - Update routes to include admin-specific endpoints with appropriate middleware.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminsAuthController;
+use App\Http\Controllers\Admin\ItemsController;
+use App\Http\Controllers\Admin\MenusCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix('admin')->group(function () {
+    Route::post('/register', [AdminsAuthController::class, 'register'])->name('admins.register');
+    Route::post('/login', [AdminsAuthController::class, 'login'])->name('admins.login');
+
+});
+Route::middleware('admin')->prefix('admin')->group(function () {
+    Route::apiResource('/menuCategories', MenusCategoryController::class);
+    Route::apiResource('/items', ItemsController::class);
+    Route::post('items/{id}/', [ItemsController::class, 'updateItem'])->name('items.uploadImage');
+    Route::post('/logout', [AdminsAuthController::class, 'logout'])->name('admins.logout');
 });
