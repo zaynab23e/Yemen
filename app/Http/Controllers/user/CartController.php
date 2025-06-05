3974a2df96471d25a235ccf87c1af8940dade125
zaynab10o <zaynabm766@gmail.com>
2025-06-05 22:01:24 +0300

    lowercased the class

--- app/Http/Controllers/user/CartController.php
+++ app/Http/Controllers/user/CartController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Cart;
+use App\Models\cart;
 use App\Models\CartItem;
 use App\Models\Item;
 use App\Models\Product;
